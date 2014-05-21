1bb517cd6ff9c8a43aabad9a4d00f9cf9e37fc8b
Chris Hall <chall@touchvisiontv.com>
2014-05-20 21:49:01 -0500

    Fullscreen

--- src/main.cpp
+++ src/main.cpp
@@ -22,8 +22,8 @@
 int main( ){
     
     ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 640*2,480*2, OF_WINDOW);			// <-------- setup the GL context
-    
+	//ofSetupOpenGL(&window, 640*2,480*2, OF_WINDOW);			// <-------- setup the GL context
+    ofSetupOpenGL(&window, 640*2, 480*2, OF_FULLSCREEN);
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
